025651cace625c0a5ce96295ad38042d27d42d65
LAGstudi0 <paolo.milutinovic@gmail.com>
2025-02-07 12:44:41 +0000

    Changes to the anchor link behaviour

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
               <section className="hero">
                   <div className="hero-left">
                     <h1>Immediate access to your full medical records and personalized health recommendations.</h1>
-                    <div><a className="find-out" href="#about">Find out how</a></div>
+                    <div><a className="find-out" href="#about" onClick={handleAnchorClick}>Find out how</a></div>
                   </div>
                   <div className="hero-container-img">
                     <img className="phone-mockup" src={Phone} alt="phone" />
